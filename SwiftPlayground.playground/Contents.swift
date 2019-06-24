2594f26c3fe54f9cfd3a575fe4f4be1f44dc3ae6
Bolaji Olajide <cooproton@gmail.com>
2019-06-24 02:22:01 +0100

    add data types

--- SwiftPlayground.playground/Contents.swift
+++ SwiftPlayground.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var str = "Hello, playground"
+var str: String = "Hello, playground!"
+//var firstname = "Bolaji"
+
+//print(str, firstname)
+
+let firstname: Any = "Bolaji"
+print(firstname)
+
+var name: String = "Proton"
+var isHungry: Bool = true
+var age: Int = 18
+var percent:Double = 0.3 // default for decimals is double
