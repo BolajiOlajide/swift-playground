4f14e19ce869eb77c929899b21038e8d887af052
Bolaji Olajide <cooproton@gmail.com>
2019-07-02 00:47:21 +0100

    WIP

--- SwiftPlayground.playground/Contents.swift
+++ SwiftPlayground.playground/Contents.swift
@@ -12,3 +12,25 @@ var name: String = "Proton"
 var isHungry: Bool = true
 var age: Int = 18
 var percent:Double = 0.3 // default for decimals is double
+
+var a = 2
+var b = 3
+var c = 4
+var d = 4.5
+
+print(a * b)
+print(a + b)
+print(c - a)
+print(pow(Decimal(a), c))
+print(sqrt(9))
+print(ceil(d)) // round up
+print(floor(d)) // round down
+
+// incrementing via shorthand
+a += 1
+// decrementing
+c -= 1
+
+if c < 5 {
+    print("c is less than 5")
+}
