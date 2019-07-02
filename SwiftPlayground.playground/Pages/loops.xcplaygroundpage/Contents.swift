e9749adfc0ae8acd81ddaf7d0bf34b0f3303b1bc
Bolaji Olajide <cooproton@gmail.com>
2019-07-02 01:10:23 +0100

    the different loops

--- SwiftPlayground.playground/Pages/loops.xcplaygroundpage/Contents.swift
+++ SwiftPlayground.playground/Pages/loops.xcplaygroundpage/Contents.swift
@@ -1,7 +1,25 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var str = "Hello, playground"
+var str: String = "Hello, playground"
 
-//: [Next](@next)
+//print("Hello")
+
+
+for counter in 1...5 {
+    print("Hello", counter)
+}
+
+var counter: Int = 5
+
+
+while counter > 0 {
+    print("hello")
+    counter -= 1
+}
+
+var counter2 = 5
+repeat {
+    print("Proton")
+    counter2 -= 1
+    
+} while counter2 > 0
